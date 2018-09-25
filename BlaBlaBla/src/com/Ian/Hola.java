356feb39cff7508d5092653b0270b27d9d527b00
IAN <IAN@LAPTOP-8OKP2EUV>
2018-09-25 16:12:03 +0800

    inital HW

---
+++ BlaBlaBla/src/com/Ian/Hola.java
@@ -0,0 +1,21 @@
+package com.Ian;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		int age=-100;
+		age=-200;
+		short bytes=8000;
+		age=bytes++;
+		float height=20000.98765f;
+		System.out.println("\"age\nbytes\nheight\"");
+		System.out.println(age);
+		System.out.println(bytes);
+		System.out.print(height);
+		
+		
+		
+
+	}
+
+}
